0ae5eea0c616c467af8ff2a534f8b4bb81baf0d4
Matheus Gois <124711079+dev-Gois@users.noreply.github.com>
2023-04-28 16:37:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,62 @@
 let tarefas = [];
 let idTarefa = 0;
 const listaTarefas = document.getElementById("lista-tarefas");
-const semTarefa = document.getElementById('semTarefa');
-const form = document.querySelector('#inserirTarefa').addEventListener("keydown", function(e){
-let valor = document.querySelector('#inserirTarefa').value;
+const semTarefa = document.getElementById("semTarefa");
+const form = document
+  .querySelector("#inserirTarefa")
+  .addEventListener("keydown", function (e) {
+    let valor = document.querySelector("#inserirTarefa").value;
 
-    if(e.key == "Enter" && valor.length !== 0) {
-        e.preventDefault();
-        criarTarefa(valor);
-        document.getElementById('inserirTarefa').value = "";
-        }
+    if (e.key == "Enter" && valor.length !== 0) {
+      e.preventDefault();
+      criarTarefa(valor);
+      document.getElementById("inserirTarefa").value = "";
     }
-)
+  });
 
 class Tarefa {
-    constructor(titulo) {
-        this.idTarefa = idTarefa++;
-        this.titulo = titulo;
+  constructor(titulo) {
+    this.idTarefa = idTarefa++;
+    this.titulo = titulo;
+  }
 }
-}
-
 
 function criarTarefa(titulo) {
-
-    let tarefa = new Tarefa(titulo);
-    tarefas.push(tarefa);
-    verificarTarefas();
-    listarTarefas(tarefas);
+  let tarefa = new Tarefa(titulo);
+  tarefas.push(tarefa);
+  definirStyle();
+  listarTarefas(tarefas);
 }
 
-function verificarTarefas() {
-    if(tarefas.length == 0) {
-        semTarefa.style.display = 'block';
-    } else {
-        semTarefa.style.display = 'none';
-    }
+function definirStyle() {
+  if (tarefas.length == 0) {
+    semTarefa.style.display = "block";
+  } else {
+    semTarefa.style.display = "none";
+  }
 }
 
 function deletarTarefa(id) {
-    tarefas.splice(tarefas.indexOf(id), 1);
-    verificarTarefas();
-    listarTarefas(tarefas);
+  tarefas.splice(tarefas.indexOf(id), 1);
+  definirStyle();
+  listarTarefas(tarefas);
 }
 
 function listarTarefas(array) {
-    listaTarefas.innerHTML = '';
-    array.forEach((tarefa) => {
-        const novaTarefa = document.createElement("li");
-        novaTarefa.innerText = tarefa.titulo;
+  listaTarefas.innerHTML = "";
+  array.forEach((tarefa) => {
+    const novaTarefa = document.createElement("li");
+    novaTarefa.innerText = tarefa.titulo;
 
-        
-        const botaoDeletar = document.createElement("img");
-        botaoDeletar.src = './delete.png';
-        botaoDeletar.alt = 'Delete';
-        botaoDeletar.addEventListener("click", () => {
-            deletarTarefa(tarefa.idTarefa);
-            novaTarefa.remove();
-        });
-
-        novaTarefa.appendChild(botaoDeletar);
-        listaTarefas.appendChild(novaTarefa);
+    const botaoDeletar = document.createElement("img");
+    botaoDeletar.src = "./delete.png";
+    botaoDeletar.alt = "Delete";
+    botaoDeletar.addEventListener("click", () => {
+      deletarTarefa(tarefa.idTarefa);
+      novaTarefa.remove();
     });
+
+    novaTarefa.appendChild(botaoDeletar);
+    listaTarefas.appendChild(novaTarefa);
+  });
 }
